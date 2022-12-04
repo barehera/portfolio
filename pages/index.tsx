c083d782d37cf7cb8893201af612ceaeee558034
Çağan Büyükavcılar <mircagi@hotmail.com>
2022-12-05 00:41:55 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ const Home: NextPage = () => {
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="py-14 px-4 bg-primary-dark min-h-screen text-white select-none overflow-hidden">
+      <main className="px-4 py-10 bg-primary-dark min-h-screen text-white select-none overflow-hidden">
         <div
           className={`${
             showScreen ? "scale-100 opacity-100" : "scale-0 opacity-0"
-          } flex flex-col overflow-hidden  justify-between max-w-[120rem] mx-auto bg-primary-light-blue rounded-lg h-[calc(100vh_-_7rem)] border border-primary-border transition-all duration-1000 ease-out`}
+          } flex flex-col overflow-hidden  justify-between max-w-[120rem] mx-auto bg-primary-light-blue rounded-lg h-[calc(100vh_-_5rem)] border border-primary-border transition-all duration-1000 ease-out`}
         >
           <Header />
           <HeroSection />
