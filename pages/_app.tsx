b56d8ebb5df216c0b3f757ebdf25c181e8b943b3
Çağan Büyükavcılar <mircagi@hotmail.com>
2022-12-12 01:44:59 +0300

    google analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ReactTooltip from "react-tooltip";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
       <ReactTooltip />
       <Component {...pageProps} />
     </>
