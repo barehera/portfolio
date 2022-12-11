baa58fbd5162fe0755ba337e15a840bc9d70c32e
Çağan Büyükavcılar <mircagi@hotmail.com>
2022-12-11 18:49:32 +0300

    animation fix

--- components/HeroSection/Hexagon.tsx
+++ components/HeroSection/Hexagon.tsx
@@ -14,6 +14,7 @@ const Hexagon = ({ image, absolute, position }: IProps) => {
         <div className={`lg:absolute ${position}`}>
           <div className="w-14 h-14 rounded-full lg:rounded-none bg-white lg:hexagon relative flex items-center justify-center hover:scale-90  duration-[2s] hover:duration-300 ease-out  ">
             <img
+              draggable="false"
               className=" w-8 h-8 lg:w-12 lg:h-12 object-contain rounded lg:rounded-lg "
               src={image}
             />
@@ -22,6 +23,7 @@ const Hexagon = ({ image, absolute, position }: IProps) => {
       ) : (
         <div className="w-14 h-14 rounded-full lg:rounded-none bg-white lg:hexagon relative flex items-center justify-center hover:scale-90  duration-[2s] hover:duration-300 ease-out  ">
           <img
+            draggable="false"
             className=" w-8 h-8 lg:w-12 lg:h-12 object-contain rounded lg:rounded-lg "
             src={image}
           />
--- components/HeroSection/SkillsSection.tsx
+++ components/HeroSection/SkillsSection.tsx
@@ -10,11 +10,47 @@ import typescriptIcon from "../../assets/typescript.png";
 import mongodbIcon from "../../assets/mongodb.png";
 import firebaseIcon from "../../assets/firebase.png";
 import Hexagon from "./Hexagon";
-import useMouseAnimation from "../../hooks/useMouseAnimation";
 
 const SkillsSection = () => {
-  const { mouseDirectionX, mouseDirectionY, rotateDirection } =
-    useMouseAnimation();
+  const [mouseDirectionX, setMouseDirectionX] = useState<string>("");
+  const [mouseDirectionY, setMouseDirectionY] = useState<string>("");
+  const [rotateDirection, setRotateDirection] = useState<string>("");
+
+  let rotated = false;
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const { movementX, movementY } = event;
+
+      if (movementX < 0 && movementY < 0) {
+        setMouseDirectionY(`lg:-translate-y-[40px]`);
+        setMouseDirectionX(`lg:-translate-x-[40px] `);
+      } else if (movementX > 0 && movementY < 0) {
+        setMouseDirectionY(`lg:-translate-y-[40px] `);
+        setMouseDirectionX(`lg:translate-x-[40px]`);
+      } else if (movementX > 0 && movementY > 0) {
+        setMouseDirectionY(`lg:translate-y-[40px]`);
+        setMouseDirectionX(`lg:translate-x-[40px]`);
+      } else if (movementX < 0 && movementY > 0) {
+        setMouseDirectionY(`lg:translate-y-[40px] `);
+        setMouseDirectionX(`lg:-translate-x-[40px] `);
+      }
+      if (rotated === false) {
+        setRotateDirection((prev) =>
+          prev === "lg:-rotate-12" ? "lg:rotate-12" : "lg:-rotate-12"
+        );
+        rotated = true;
+        setTimeout(() => {
+          rotated = false;
+        }, 2000);
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
 
   return (
     <div className=" relative flex lg:items-center lg:justify-center h-full lg:col-span-7 ">
